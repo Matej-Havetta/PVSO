a281fcdb80231cb09e1170b9c91d04130fe60678
Matej-Havetta <xhavetta@stuba.sk>
2025-02-18 23:57:08 +0100

    fix

--- camera_test.py
+++ camera_test.py
@@ -11,7 +11,7 @@ cam = xiapi.Camera()
 #to open specific device, use:
 #cam.open_device_by_SN('41305651')
 #(open by serial number)
-print('Opening first camera...')
+print('Opening first camerecho 0|sudo tee /sys/module/usbcore/parameters/usbfs_memory_mba...')
 cam.open_device()
 
 #settings
@@ -27,14 +27,19 @@ img = xiapi.Image()
 print('Starting data acquisition...')
 cam.start_acquisition()
 
-
-while cv2.waitKey() != ord('q'):
+k = 0
+while k != 4:
     cam.get_image(img)
-    image = img.get_image_data_numpy()
-    image = cv2.resize(image,(240,240))
-    cv2.imshow("test", image)
-    cv2.waitKey()
-
+    frame = img.get_image_data_numpy()
+    frame = cv2.resize(frame, (240,240))
+    cv2.imshow("camera window", frame)
+    if cv2.waitKey(1) == ord(" "):
+        cam.get_image(img)
+        image = img.get_image_data_numpy()
+        image = cv2.resize(image,(240,240))
+        cv2.imshow("img{}".format(k+1), image)
+        cv2.imwrite("images/img{}.jpg".format(k+1), image)
+        k += 1
 #stop data acquisition
 print('Stopping acquisition...')
 cam.stop_acquisition()
