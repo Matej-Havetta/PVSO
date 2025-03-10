f9fbd52a10fe13de9d15d17413702a2cbf5f98fc
Filip Kolenčik <xkolencikf@stuba.sk>
2025-03-10 14:52:25 +0100

    Almost there

--- Zadanie_2/Zadanie_2.py
+++ Zadanie_2/Zadanie_2.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from ximea import xiapi
+import glob
 
 # Inicializácia kamery XIMEA
 cam = xiapi.Camera()
@@ -18,67 +19,90 @@ def capture_image():
 
 
 # Funkcia na kalibráciu kamery pomocou šachovnice
-def calibrate_camera(image_files, pattern_size=(9, 6), square_size=0.025):
-    obj_points = []
-    img_points = []
-    objp = np.zeros((pattern_size[0] * pattern_size[1], 3), np.float32)
-    objp[:, :2] = np.mgrid[0:pattern_size[0], 0:pattern_size[1]].T.reshape(-1, 2)
+def calibrate_camera(images, chessboardSize = (5, 7), square_size=0.025):
+    # termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    objp = np.zeros((chessboardSize[0] * chessboardSize[1], 3), np.float32)
+    objp[:, :2] = np.mgrid[0:chessboardSize[0], 0:chessboardSize[1]].T.reshape(-1, 2)
     objp *= square_size
 
-    for fname in image_files:
-        img = cv2.imread(fname)
+    # Arrays to store object points and image points from all the images.
+    obj_points = []  # 3d point in real world space
+    img_points = []  # 2d points in image plane.
+
+    for image in images:
+        img = cv2.imread(image)
         if img is None:
-            print(f"Error: Unable to read image file {fname}")
+            print(f"Error: Unable to read image file {image}")
             continue
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        ret, corners = cv2.findChessboardCorners(gray, pattern_size, None)
+        ret, corners = cv2.findChessboardCorners(gray, chessboardSize, None)
         if ret:
-            img_points.append(corners)
             obj_points.append(objp)
+            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            img_points.append(corners2)
+
+            # Draw and display the corners
+            cv2.drawChessboardCorners(img, chessboardSize, corners2, ret)
+            cv2.imshow('img', img)
+            cv2.waitKey(0)
         else:
-            print(f"Warning: Chessboard corners not found in image {fname}")
+            print(f"Warning: Chessboard corners not found in image {image}")
 
     if not img_points:
         raise ValueError("Error: No chessboard corners found in any image.")
 
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, gray.shape[::-1], None, None)
-    if not ret:
-        raise RuntimeError("Error: Camera calibration failed.")
-
-    fx = mtx[0, 0]
-    fy = mtx[1, 1]
-    cx = mtx[0, 2]
-    cy = mtx[1, 2]
-
-    print(f'Camera Matrix:\n{mtx}')
-    print(f'fx: {fx}, fy: {fy}, cx: {cx}, cy: {cy}')
-    return mtx, dist
+    ret, cameraMatrix, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, gray.shape[::-1], None, None)
+    print("cameraMatrix:", cameraMatrix)
+    print("fx:", cameraMatrix.item(0, 0), "fy:", cameraMatrix.item(1, 1))
+    print("cx:", cameraMatrix.item(0, 2), "cy:", cameraMatrix.item(1, 2))
 
 
 # Funkcia na detekciu kruhov pomocou Hough Transformácie
-def detect_circles(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray = cv2.medianBlur(gray, 5)
-    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, dp=1.2, minDist=30, param1=50, param2=30, minRadius=10,
-                               maxRadius=100)
-
+def detect_circles(image, minDist=30, param1=50, param2=30, minRadius=10, maxRadius=100):
+    image = cv2.medianBlur(image, 5)
+    cimg = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    circles = cv2.HoughCircles(image, cv2.HOUGH_GRADIENT, dp=1.2, minDist=minDist, param1=param1, param2=param2,
+                               minRadius=minRadius, maxRadius=maxRadius)
+    output = cimg.copy()
     if circles is not None:
         circles = np.uint16(np.around(circles))
         for i in circles[0, :]:
-            cv2.circle(image, (i[0], i[1]), i[2], (0, 255, 0), 2)
-            cv2.circle(image, (i[0], i[1]), 2, (0, 0, 255), 3)
-            print(f'Kruh - Stred: ({i[0]}, {i[1]}), Priemer: {2 * i[2]} px')
-    return image
+            cv2.circle(output, (i[0], i[1]), i[2], (0, 255, 0), 2)
+            cv2.circle(output, (i[0], i[1]), 2, (0, 0, 255), 3)
+    return output
 
+def nothing(x):
+    pass
 
 # Hlavný kód
-frame = capture_image()
-images = ['img1.jpg','img2.jpg','img3.jpg','img4.jpg','img5.jpg','img6.jpg','img7.jpg','img8.jpg','img9.jpg','img10.jpg','img11.jpg','img12.jpg']
-mtx, dist = calibrate_camera(images)
-detected_circles = detect_circles(frame)
-cv2.resizeWindow('Detected Circles', 800, 600)
-cv2.imshow('Detected Circles', detected_circles)
-cv2.waitKey(0)
+images = glob.glob('*.jpg')
+calibrate_camera(images)
+
+cv2.namedWindow('Detected Circles')
+cv2.createTrackbar('Min Distance', 'Detected Circles', 30, 200, nothing)
+cv2.createTrackbar('Param1', 'Detected Circles', 50, 200, nothing)
+cv2.createTrackbar('Param2', 'Detected Circles', 30, 200, nothing)
+cv2.createTrackbar('Min Radius', 'Detected Circles', 10, 100, nothing)
+cv2.createTrackbar('Max Radius', 'Detected Circles', 100, 200, nothing)
+
+while True:
+    cv2.imshow('Detected Circles', capture_image())
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    minDist = cv2.getTrackbarPos('Min Distance', 'Detected Circles')
+    param1 = cv2.getTrackbarPos('Param1', 'Detected Circles')
+    param2 = cv2.getTrackbarPos('Param2', 'Detected Circles')
+    minRadius = cv2.getTrackbarPos('Min Radius', 'Detected Circles')
+    maxRadius = cv2.getTrackbarPos('Max Radius', 'Detected Circles')
+    frame = capture_image()
+    if frame is not None:
+        output = detect_circles(frame,minDist, param1, param2, minRadius, maxRadius)
+        cv2.imshow('Detected Circles', output)
+    else:
+        print("Error: No image captured from camera.")
+
 cv2.destroyAllWindows()
 
 # Ukončenie kamery
