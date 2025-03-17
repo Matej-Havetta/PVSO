03598ef6c7f28910f32924946c1c8b5136a89286
Filip Kolenčik <xkolencikf@stuba.sk>
2025-03-17 14:41:42 +0100

    Dokončené Zadanie2

--- Zadanie_2/Zadanie_2.py
+++ Zadanie_2/Zadanie_2.py
@@ -15,6 +15,7 @@ def capture_image():
     img = xiapi.Image()
     cam.get_image(img)
     frame = img.get_image_data_numpy()
+    frame = cv2.resize(frame, (640, 480))
     return frame
 
 
@@ -60,18 +61,32 @@ def calibrate_camera(images, chessboardSize = (5, 7), square_size=0.025):
 
 
 # Funkcia na detekciu kruhov pomocou Hough Transformácie
-def detect_circles(image, minDist=30, param1=50, param2=30, minRadius=10, maxRadius=100):
-    image = cv2.medianBlur(image, 5)
-    cimg = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-    circles = cv2.HoughCircles(image, cv2.HOUGH_GRADIENT, dp=1.2, minDist=minDist, param1=param1, param2=param2,
+def detect_circles(image, minDist=30, param1=100, param2=60, minRadius=10, maxRadius=150):
+
+    if len(image.shape) == 3:  # Má obraz 3 rozmery? (výška, šírka, kanály)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    else:
+        gray = image  # Ak už je grayscale, použijeme ho priamo
+
+    clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
+    gray = clahe.apply(gray)
+
+    gray = cv2.GaussianBlur(gray, (9, 9), 2, 2)
+
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, dp=1.2, minDist=minDist, param1=param1, param2=param2,
                                minRadius=minRadius, maxRadius=maxRadius)
-    output = cimg.copy()
+    # dp upravuje rozlíšenie, minDist je minimálna vzdialenosť medzi stredmi detekovaných kruhov
+    # param1 Toto je horný prah pre detekciu hrán pomocou Cannyho detekcie hrán.
+    # param2 Určuje minimálne skóre, ktoré musí kandidátsky kruh dosiahnuť, aby bol akceptovaný ako skutočný kruh.
+    # minRadius a maxRadius sú minimálny a maximálny polomer kruhov
+
     if circles is not None:
         circles = np.uint16(np.around(circles))
         for i in circles[0, :]:
-            cv2.circle(output, (i[0], i[1]), i[2], (0, 255, 0), 2)
-            cv2.circle(output, (i[0], i[1]), 2, (0, 0, 255), 3)
-    return output
+            cv2.circle(image, (i[0], i[1]), i[2], (0, 255, 0), 2)
+            cv2.circle(image, (i[0], i[1]), 2, (0, 0, 255), -1)
+
+    return image
 
 def nothing(x):
     pass
@@ -82,23 +97,24 @@ calibrate_camera(images)
 
 cv2.namedWindow('Detected Circles')
 cv2.createTrackbar('Min Distance', 'Detected Circles', 30, 200, nothing)
-cv2.createTrackbar('Param1', 'Detected Circles', 50, 200, nothing)
-cv2.createTrackbar('Param2', 'Detected Circles', 30, 200, nothing)
+cv2.createTrackbar('Param1', 'Detected Circles', 100, 200, nothing)
+cv2.createTrackbar('Param2', 'Detected Circles', 60, 200, nothing)
 cv2.createTrackbar('Min Radius', 'Detected Circles', 10, 100, nothing)
-cv2.createTrackbar('Max Radius', 'Detected Circles', 100, 200, nothing)
+cv2.createTrackbar('Max Radius', 'Detected Circles', 150, 200, nothing)
 
 while True:
-    cv2.imshow('Detected Circles', capture_image())
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(10) & 0xFF == ord('q'):
         break
-    minDist = cv2.getTrackbarPos('Min Distance', 'Detected Circles')
-    param1 = cv2.getTrackbarPos('Param1', 'Detected Circles')
-    param2 = cv2.getTrackbarPos('Param2', 'Detected Circles')
-    minRadius = cv2.getTrackbarPos('Min Radius', 'Detected Circles')
-    maxRadius = cv2.getTrackbarPos('Max Radius', 'Detected Circles')
+    minDist = max(cv2.getTrackbarPos('Min Distance', 'Detected Circles'), 1)
+    param1 = max(cv2.getTrackbarPos('Param1', 'Detected Circles'), 1)
+    param2 = max(cv2.getTrackbarPos('Param2', 'Detected Circles'), 1)
+    minRadius = max(cv2.getTrackbarPos('Min Radius', 'Detected Circles'), 1)
+    maxRadius = max(cv2.getTrackbarPos('Max Radius', 'Detected Circles'), 1)
     frame = capture_image()
     if frame is not None:
-        output = detect_circles(frame,minDist, param1, param2, minRadius, maxRadius)
+        output = frame.copy()
+        output = detect_circles(frame, minDist, param1, param2, minRadius, maxRadius)
+        cv2.resizeWindow('Detected Circles',(640, 480))
         cv2.imshow('Detected Circles', output)
     else:
         print("Error: No image captured from camera.")
